b8651b6640104b9b2f168352bc8383d203047634
Jialiang Han <hanjiali@umich.edu>
2021-09-19 21:33:50 +0800

    still working header and cpp file

--- genetic_algorithm/genetic_algorithm.cpp
+++ genetic_algorithm/genetic_algorithm.cpp
@@ -2,9 +2,10 @@
 //exmaple is to find max value of -(x^2)/10 +3x, x in [0,31]
 
 #include "genetic_algorithm.h"
-
-
-
+#include <bitset>
+#include <math.h>
+#include <iostream>
+#include <random>
 namespace genetic_algorithm
 {
 
@@ -18,14 +19,117 @@ namespace genetic_algorithm
         return initial_generation;
     }
 
-    encoded_generation encoding(generation x)
+    encoded_generation genetic_algorithm::encoding(generation& x)
     {
-        std::string encoded="";
         encoded_generation encoded_generation;
         for(auto i:x)
         {
-            std::itoa(i,encoded,2);
+            std::string encoded = std::bitset< 5 >( i ).to_string();
             encoded_generation.emplace(i,encoded);
         }
+        return encoded_generation;
     }
+
+    generation genetic_algorithm::decoding(encoded_generation& y)
+    {
+        generation generation;
+        for(auto i:y)
+        {
+            generation.push_back(i.first);
+        }
+        return generation;
+    }
+
+    double genetic_algorithm::fitness_function(int x)
+    {
+        double result;
+        result = -(x * x)/10 + 3*x;
+        return result;
+    }
+
+    fitness_map genetic_algorithm::fitness(encoded_generation& y)
+    {
+        fitness_map map;
+        for(auto i:y)
+        {
+            map.emplace(i.second,fitness_function(i.first));
+        }
+        return map;
+    }
+        
+    int genetic_algorithm::string_to_int(std::string s)
+    {
+        int size = s.size();
+        int result=0;
+        for(int i=0;i<size;++i)
+        {
+            result += (s[i]-48) * pow(2,size-i-1);
+        }
+        return result;
+    }
+
+    encoded_generation genetic_algorithm::selection(fitness_map& z)
+    {
+        float total_fitness=0;
+        selection_map probability_map;
+        encoded_generation selected;
+        for(auto i:z)
+        {
+            total_fitness += i.second;
+        }
+        for(auto i:z)
+        {
+            probability_map.emplace(i.second/total_fitness, i.first);
+        }
+        auto j=probability_map.lower_bound(0.10);
+        for(j;j!=probability_map.end();++j)
+        {
+            selected.emplace(string_to_int(j->second),j->second);
+        }
+        return selected;
+    }
+
+    encoded_generation genetic_algorithm::mating(encoded_generation& z)
+    {   
+        encoded_generation next;
+        int size = z.size();
+        std::default_random_engine generator;
+        std::uniform_int_distribution<int> distribution(0,size);
+        for(int i=0;i<population_size;++i)
+        {
+            int dice_roll = distribution(generator);
+            auto random_it = std::next(std::begin(z), dice_roll);
+            next.emplace(random_it->first,random_it->second);
+        }
+        return next;
+    }
+
+    encoded_generation exchange_genes(encoded_generation& encoded_generation)
+    {
+        
+    }
+
+    generation genetic_algorithm::generate_next(encoded_generation& generation)
+    {
+        //mating
+        //select location to swap genes
+        //mutation
+    }
+
+    
+
+}
+
+int main(int argc, char** argv) {
+    int max_iterations =1000;
+    double probability_mutation = 0.01;
+    int population_size = 10;
+    genetic_algorithm::genetic_algorithm test(max_iterations,probability_mutation,population_size);
+    generation initial_generation;
+    initial_generation=test.initial_generation();
+    for(auto j=initial_generation.begin();j!=initial_generation.end();++j)
+    {
+            std::cout << "value: " << (*j) <<std::endl;
+    }
+    return 0;
 }
--- genetic_algorithm/genetic_algorithm.h
+++ genetic_algorithm/genetic_algorithm.h
@@ -1,14 +1,16 @@
+
 #ifndef GENETIC_ALGORITHM
 #define GENETIC_ALGORITHM
 
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <map>
 
 typedef std::vector<int> generation;
 typedef std::unordered_map<int,std::string> encoded_generation;
 typedef std::unordered_map<std::string,double> fitness_map;
-typedef std::unordered_map<std::string,float> selection_map; 
+typedef std::map<float,std::string> selection_map; 
 
 namespace genetic_algorithm
 {
@@ -16,21 +18,104 @@ namespace genetic_algorithm
 class genetic_algorithm
 {
 public:
-    genetic_algorithm();
+    // genetic_algorithm();
+    /**
+     * @brief Construct a new genetic algorithm object
+     * 
+     * @param max_iterations 
+     * @param probability_mutation 
+     * @param population_size 
+     */
     genetic_algorithm(int max_iterations,double probability_mutation,int population_size)
     {
         max_iteration = max_iterations;
         probability_mutation = probability_mutation;
         population_size = population_size;
     };
-    ~genetic_algorithm();
+    // ~genetic_algorithm();
+    /**
+     * @brief generate initial generation randomly
+     * 
+     * @return generation 
+     */
     generation initial_generation();
-    encoded_generation encoding(generation x);
-    generation decoding(encoded_generation y);
-    fitness_map fitness(encoded_generation y);
-    encoded_generation selection(fitness_map z);
-    generation generate_next(encoded_generation generation);
+    /**
+     * @brief encoding generation into encoded generation by defined encoding method
+     * 
+     * @param x : a generation
+     * @return encoded_generation 
+     */
+    encoded_generation encoding(generation& x);
+    /**
+     * @brief decoding encoding generation into generation
+     * 
+     * @param y a encoded generation
+     * @return generation 
+     */
+    generation decoding(encoded_generation& y);
+    /**
+     * @brief fitness function
+     * 
+     * @param x 
+     * @return double 
+     */
+    double fitness_function(int x);
+    /**
+     * @brief convert a string type binary code into a decimal int
+     * 
+     * @param s a binary int in string type
+     * @return int 
+     */
+    int string_to_int(std::string s);
+    /**
+     * @brief generate a fitness map using fitness function
+     * 
+     * @param y encoded generation
+     * @return fitness_map 
+     */
+    fitness_map fitness(encoded_generation& y);
+    /**
+     * @brief select some chrono accoriding some crieteria
+     * 
+     * @param z fitness map
+     * @return encoded_generation 
+     */
+    encoded_generation selection(fitness_map& z);
+    /**
+     * @brief randomly mate two chrono to get a full size generation 
+     * 
+     * @param z encoded generation after selection 
+     * @return encoded_generation 
+     */
+    encoded_generation mating(encoded_generation& z);
+    /**
+     * @brief exchange genes at some random location
+     * 
+     * @param encoded_generation 
+     * @return encoded_generation 
+     */
+    encoded_generation exchange_genes(encoded_generation& encoded_generation);
+    /**
+     * @brief mutate a chrono by some probability
+     * 
+     * @param chrono 
+     * @return std::string 
+     */
     std::string mutation(std::string chrono);
+    /**
+     * @brief use mutation function for  generation to mutate full generation
+     * 
+     * @param g a generation
+     * @return generation 
+     */
+    generation generation_mutated(generation& g);
+    /**
+     * @brief generate next generation by selection, mating and mutation
+     * 
+     * @param generation 
+     * @return generation 
+     */
+    generation generate_next(encoded_generation& generation);
    
 private:
     int max_iteration;
